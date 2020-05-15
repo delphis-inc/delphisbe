a83936872e033bbb648555a2f524410d6ff43d48
Tim Harper <tim.harper598@gmail.com>
2020-05-15 15:50:28 -0700

    already ran

--- _scripts/migrations/0009_add_quoted_post_id.sql
+++ _scripts/migrations/0009_add_quoted_post_id.sql
@@ -1,1 +1,2 @@
 ALTER TABLE posts ADD COLUMN IF NOT EXISTS quoted_post_id varchar(36);
+ALTER TABLE posts ADD CONSTRAINT posts_quoted_post_id_eaa15cd7531b FOREIGN KEY (quoted_post_id) REFERENCES posts (id) MATCH FULL;
